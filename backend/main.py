9f6fe422b3b49395af4642055dfa2e85af91207d
William D C Santana <williamdiegocampos@gmail.com>
2020-05-02 12:05:31 -0300

    [3] Retornando endpoint mock sem tratamento no response

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 # main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import json
 
 app = FastAPI()
 
@@ -7,3 +8,15 @@ app = FastAPI()
 @app.get('/hello')
 def get_hello_world():
     return {"hello" : "world"}
+
+
+@app.get('/noticias/mock'
+,responses={200:{}
+            ,404:{"description":"Mock não encontrado"}}
+)
+def noticias_mock():
+    try:
+        with open('response_mock.json', 'r') as json_mock:
+            return json.load(json_mock)
+    except:
+        raise HTTPException(status_code=404, detail='Mock não encontrado')
