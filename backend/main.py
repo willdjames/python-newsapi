85c349957317f4331903e2b68a47199260833c74
William D C Santana <williamdiegocampos@gmail.com>
2020-05-04 22:42:21 -0300

    [4] api noticas do brasil

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 # main.py
 from fastapi import FastAPI, HTTPException
+from newsapi import NewsApiClient
 import json
 
 app = FastAPI()
@@ -20,3 +21,15 @@ def noticias_mock():
             return json.load(json_mock)
     except:
         raise HTTPException(status_code=404, detail='Mock não encontrado')
+
+
+@app.get('/noticias'
+        ,responses={200:{}}
+)
+def noticias_br():
+    '''
+    Top notícias do Brasil
+    '''
+    newsapi = NewsApiClient(api_key='dd65faf4401a4b8191d81de5d622de1c')
+
+    return newsapi.get_top_headlines(country='br')
